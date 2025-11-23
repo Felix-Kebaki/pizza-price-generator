4cc8c56cb0aa3c40fb6ec0a4d2295c09cbfd0cd9
Felix Kebaki <felixkebaki@gmail.com>
2025-11-23 09:36:29 +0300

    Final files

--- src/com/priceGenerator/Options.java
+++ src/com/priceGenerator/Options.java
@@ -9,17 +9,8 @@ public class Options {
 
     public Options() {
         System.out.println("\nWelcome to Pizza place.\n\nPick your order below.[1 or 2]");
-        System.out.println("1.Pizza Vegetable ---Ksh800   2.Pizza Non-vegetable ---Ksh1000");
-        System.out.print("Enter: ");
-        int firstPick = scanner.nextInt();
 
-        if (firstPick == 1 || firstPick == 2) {
-            pizza.checkIfVeg(firstPick);
-        } else {
-            System.out.println("Invalid input!!!");
-            System.exit(1);
-        }
-
+        getPizzaResponse();
         getCheeseResponse();
         getToppingResponse();
         getTakeawayResponse();
@@ -27,51 +18,93 @@ public class Options {
         pizza.getBill();
     }
 
+    public void getPizzaResponse() {
+        System.out.println("1.Pizza Vegetable ---Ksh800   2.Pizza Non-vegetable ---Ksh1000");
+        while (true) {
+            System.out.print("Enter: ");
+            if (scanner.hasNextInt()) {
+
+                int firstPick = scanner.nextInt();
+
+                if (firstPick == 1 || firstPick == 2) {
+                    pizza.checkIfVeg(firstPick);
+                    break;
+                } else {
+                    System.out.println("\nInvalid input! Please enter 1 or 2.");
+                }
+            } else {
+                System.out.println("\nOnly numbers allowed! Try again.");
+                scanner.next();
+            }
+        }
+    }
+
     public void getCheeseResponse() {
         System.out.println("\nPick your preference:[1 or 2]");
         System.out.println("1.Extra cheese ---Ksh200     2.No extra cheese");
-        System.out.print("Enter: ");
-        int secondPick = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter: ");
+            if (scanner.hasNextInt()) {
+                int secondPick = scanner.nextInt();
 
-        if (secondPick == 1) {
-            pizza.addExtraCheese();
-        } else if (secondPick == 2) {
-
-        } else {
-            System.out.println("\nInvalid input!!!");
-            getCheeseResponse();
+                if (secondPick == 1) {
+                    pizza.addExtraCheese();
+                    break;
+                } else if (secondPick == 2) {
+                    break;
+                } else {
+                    System.out.println("\nInvalid input! Please enter 1 or 2.");
+                }
+            } else {
+                System.out.println("\nOnly numbers allowed! Try again.");
+                scanner.next();
+            }
         }
     }
 
     public void getToppingResponse() {
         System.out.println("\nPick your preference:[1 or 2]");
         System.out.println("1.Extra topping ---Ksh100     2.No extra topping");
-        System.out.print("Enter: ");
-        int thirdPick = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter: ");
+            if (scanner.hasNextInt()) {
+                int thirdPick = scanner.nextInt();
 
-        if (thirdPick == 1) {
-            pizza.addExtraTopping();
-        } else if (thirdPick == 2) {
-
-        } else {
-            System.out.println("\nInvalid input!!!");
-            getToppingResponse();
+                if (thirdPick == 1) {
+                    pizza.addExtraTopping();
+                    break;
+                } else if (thirdPick == 2) {
+                    break;
+                } else {
+                    System.out.println("\nInvalid input! Please enter 1 or 2.");
+                }
+            } else {
+                System.out.println("\nOnly numbers allowed! Try again.");
+                scanner.next();
+            }
         }
     }
 
     public void getTakeawayResponse() {
         System.out.println("\nPick your preference:[1 or 2]");
         System.out.println("1.Take away ---Ksh50     2.Eat here");
-        System.out.print("Enter: ");
-        int fourthPick = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter: ");
+            if (scanner.hasNextInt()) {
+                int fourthPick = scanner.nextInt();
 
-        if (fourthPick == 1) {
-            pizza.takeAway();
-        } else if (fourthPick == 2) {
-
-        } else {
-            System.out.println("\nInvalid input!!!");
-            getTakeawayResponse();
+                if (fourthPick == 1) {
+                    pizza.takeAway();
+                    break;
+                } else if (fourthPick == 2) {
+                    break;
+                } else {
+                    System.out.println("\nInvalid input! Please enter 1 or 2.");
+                }
+            } else {
+                System.out.println("\nOnly numbers allowed! Try again.");
+                scanner.next();
+            }
         }
     }
 }
